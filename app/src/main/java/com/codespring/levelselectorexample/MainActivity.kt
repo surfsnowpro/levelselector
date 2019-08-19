a34ed49fbae3d8d1fba24774febc8fd7f3a05ff6
James Hartman <jimmy@spiredigital.com>
2019-08-18 20:25:49 -0600

    Update to test selection listener

--- app/src/main/java/com/codespring/levelselectorexample/MainActivity.kt
+++ app/src/main/java/com/codespring/levelselectorexample/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.codespring.levelselectorexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.codespring.levelselector.LevelSelector
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
             buttonHeight = 40
             buttonWidth = 40
             buttonRadius = 40f
+        }
+        volumeControl.levelSelectionListener = { index, selectedIndex, max ->
+            Log.d(TAG, "Clicked item $index. Current selection is $selectedIndex or $max")
         }
         main_content.addView(volumeControl)
     }
